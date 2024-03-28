2d2ef00b7043debfa34648076c54df919b0e752f
JoaoNovais181 <pg53907@alunos.uminho.pt>
2024-03-28 14:10:37 +0000

    Inicio exercicio lock

--- Erlang/lock.erl
+++ Erlang/lock.erl
@@ -1,9 +1,37 @@
 -module(lock).
 -export([]).
 
-create()
+create() -> 
+  spawn(fun() -> lockLoop(([],[])) end).
 
-% Main Manager
+rpc(Lock, Request) ->
+  Lock ! {Request, Lock},
+  receive {Res, Lock} -> Res end.
 
-loop(List) ->
-  loop(List).
+init(Lock, Peers) ->
+  rpc(Lock, {init, Peers}).
+
+acquire(Lock) -> 
+  rpc(Lock, {acquire}).
+
+release(Lock) -> 
+  rpc(Lock, {release}).
+
+stop(Lock) ->
+  rpc(Lock, {stop}).
+
+% lock code
+
+handle({init, Peers}, (Msgs, CurrentPeers)) ->
+  {ok, (Msgs, Peers)}.
+
+handle({acquire}, (Msgs, ))
+
+lockLoop(State) ->
+  receive
+    {stop, From} -> From ! {ok, self()}. 
+    {Request, From} ->
+      {Res, NextState} = handle(Request, (State)),
+      From ! {Res, self()},
+      lockLoop(NextState)
+  end.
